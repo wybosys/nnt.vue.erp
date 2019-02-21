f0dd622c11935792c355fad48943f260e29fdd54
wybosys <wybosys@gmail.com>
2019-02-21 17:47:45 +0800

    sub

--- src/components/manage/authConfig.ts
+++ src/components/manage/authConfig.ts
@@ -1,16 +1,15 @@
 // import replenishment from "*.vue";
 
 export const navMenu = {
-  admin:{
-    ctrName:"基础数据后台",
-    action:{
+  admin: {
+    ctrName: "基础数据后台",
+    action: {
       'TableUseSample': '表格样例',
       'ChartUseSample': '图表样例',
     }
   },
-  play:{
+  play: {
     ctrName: "玩法数据管理",
-    action:{
-    }
+    action: {}
   },
 }
