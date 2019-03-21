51fff9536fbae8d6b67e9eedd4627b11126b804f
wybosys <wybosys@gmail.com>
2019-03-21 18:08:09 +0800

    fix kp

--- src/nnt/model/table/Cell.ts
+++ src/nnt/model/table/Cell.ts
@@ -1,4 +1,5 @@
 import {IProperty, Property} from "../base/Property";
+import {ObjectT} from "../../core/Kernel";
 
 export interface ICell extends IProperty {
 
@@ -9,6 +10,12 @@ export class Cell extends Property implements Cell {
   static Value(value: any): Cell {
     let r = new this();
     r.value = value;
+    return r;
+  }
+
+  static KeyPath(value: any, kp: string): Cell {
+    let r = new this();
+    r.value = ObjectT.GetValueByKeyPath(value, kp);
     return r;
   }
 
