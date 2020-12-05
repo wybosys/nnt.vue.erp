0bc008c125653e0b7c79ec6ecc2bfc7c76ca7865
wybosys <wybosys@gmail.com>
2020-12-05 16:26:42 +0800

    fix

--- src/app/Main.vue
+++ src/app/Main.vue
@@ -19,6 +19,7 @@ import {RestSession} from "../nnt/core/RestSession";
 import {SignalFailed} from "../nnt/core/Signals";
 import {Model} from "../nnt/core/ApiModel";
 import {Application} from "../nnt/erp/Application";
+import {STATUS} from "../nnt/core/Model";
 
 export default {
   name: 'Main',
@@ -30,9 +31,18 @@ export default {
     // 显示基础错误信息
     RestSession.signals.connect(SignalFailed, s => {
       let data: Model = s.data
-      let content = 'RestSession fail:\nAPI ' + data.action + ' 返回 ' + JSON.stringify(data.response)
-      alert(content)
-    }, null);
+      switch (data.code) {
+        case STATUS.OVERFLOW: {
+          this.$message.info("数据量巨大，请设置筛选条件后重新查询")
+        }
+          break
+        default: {
+          let content = 'API请求失败:\nAPI ' + data.action + ' 返回 ' + JSON.stringify(data.response)
+          this.$message.error(content)
+        }
+          break
+      }
+    }, null)
   },
   methods: {
     actExit() {
@@ -43,37 +53,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .main-wrap {
-    position: relative;
-    width: 100vw;
-    height: 100vh;
-  }
+.main-wrap {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
 
-  .main-header {
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    height: 80px;
-    background-color: $color-base;
-    color: #fff;
-    text-align: center;
-    padding: 0 60px;
+.main-header {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 80px;
+  background-color: $color-base;
+  color: #fff;
+  text-align: center;
+  padding: 0 60px;
 
-    .title {
-      font: 40px/80px 'Microsoft YaHei UI';
-    }
+  .title {
+    font: 40px/80px 'Microsoft YaHei UI';
   }
+}
 
-  .main-container {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    top: 80px;
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-  }
+.main-container {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 80px;
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+}
 
 </style>
